a9cddf75fe2e424c0fff0d257b68693df87ebab5
Newton dos Santos <neewrobert@gmail.com>
2023-05-23 17:16:44 +0200

    add measurement of the PrimeNumberService
    This measurements if for getting the execution time

--- src/main/kotlin/com/neewrobert/helidonmp/PrimeNumberService.kt
+++ src/main/kotlin/com/neewrobert/helidonmp/PrimeNumberService.kt
@@ -1,48 +1,62 @@
 package com.neewrobert.helidonmp
 
 import jakarta.enterprise.context.RequestScoped
+import kotlin.system.measureTimeMillis
 
 @RequestScoped
 open class PrimeNumberService {
 
     //using sieveOfEratosthenes algorithm
     open fun getPrimeNumbersInRange(rangeLimit: Int): List<Int> {
-
-        val isPrime = BooleanArray(rangeLimit + 1) { true }
         val primeNumbers = mutableListOf<Int>()
+        measureTimeMillis {
+            println("getPrimeNumbersInRange()")
 
-        var currentNumber = 2
-        while (currentNumber * currentNumber <= rangeLimit) {
-            if (isPrime[currentNumber]) {
-                for (multiple in currentNumber * currentNumber..rangeLimit step currentNumber) {
-                    isPrime[multiple] = false
+            val isPrime = BooleanArray(rangeLimit + 1) { true }
+
+
+            var currentNumber = 2
+            while (currentNumber * currentNumber <= rangeLimit) {
+                if (isPrime[currentNumber]) {
+                    for (multiple in currentNumber * currentNumber..rangeLimit step currentNumber) {
+                        isPrime[multiple] = false
+                    }
+                }
+                currentNumber++
+            }
+
+            for (number in 2..rangeLimit) {
+                if (isPrime[number]) {
+                    primeNumbers.add(number)
                 }
             }
-            currentNumber++
-        }
-
-        for (number in 2..rangeLimit) {
-            if (isPrime[number]) {
-                primeNumbers.add(number)
-            }
+        }.also {
+            println("PrimeNumberService.getPrimeNumbersInRange() took $it ms")
         }
 
         return primeNumbers
     }
 
     open fun isPrimeNumber(number: Int): Boolean {
-
         if (number <= 1) {
             return false
         }
-        var currentNumber = 2
-        while (currentNumber * currentNumber <= number) {
-            if (number % currentNumber == 0) {
-                return false
-            }
-            currentNumber++
-        }
-        return true
-    }
 
+        var isPrime = true
+        measureTimeMillis {
+            println("isPrimeNumber()")
+            var currentNumber = 2
+            while (currentNumber * currentNumber <= number) {
+                if (number % currentNumber == 0) {
+                    isPrime = false
+                    break
+                }
+                currentNumber++
+            }
+        }.also {
+            println("isPrimeNumber() took $it ms")
+        }
+
+        return isPrime
+    }
 }
